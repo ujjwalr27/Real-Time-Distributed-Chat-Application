57609a5499e545986b4ee91d77102d0af86a2429
ujjwalr27 <ujjwal3654@gmail.com>
2025-01-31 23:46:26 +0530

    f

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 app_name = 'chatapp'
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
     path('', login_required(views.index), name='index'),
     path('<str:room_name>/', login_required(views.room), name='room'),
 ]
--- chatapp/views.py
+++ chatapp/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Room, Message
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Account created successfully!')
+            return redirect('chatapp:index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 @login_required
 def index(request):
