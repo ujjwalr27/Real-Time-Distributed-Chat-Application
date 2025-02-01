58930300379efaf16e06c47689ad3774d295bb82
ujjwalr27 <ujjwal3654@gmail.com>
2025-02-02 01:02:15 +0530

    lo

--- build.sh
+++ build.sh
@@ -5,10 +5,14 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Create static directory if it doesn't exist
+mkdir -p static
+
 # Collect static files
 python manage.py collectstatic --no-input
 
-# Apply database migrations
+# Make and apply migrations
+python manage.py makemigrations
 python manage.py migrate
 
 # Create cache table for sessions
--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -9,24 +9,20 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
+
+# Set the Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.production_settings')
+
+# Initialize Django ASGI application early to ensure the AppRegistry is populated
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from chatapp.routing import websocket_urlpatterns
 
-# Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.production_settings')
-
-# Import Django and set it up
-import django
-django.setup()
-
-# Import after Django setup
-from chatapp.consumers import ChatConsumer
-from django.urls import path
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
